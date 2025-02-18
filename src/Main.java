90ac0ad6cf913193ed73baf6338b9bfd68f549ff
Samirjon07 <mr.samirjon.07@gmail.com>
2025-02-18 22:34:35 +0500

    Commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b;
+        b=6 + a++;
+        System.out.println("b is " + (b));
+        System.out.println("a is " + (a++));
+    }
+}
+
+        
