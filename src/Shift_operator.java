aa66737c2e031399c3f5dae1040c57ba89d6d745
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-02-19 23:07:57 +0500

    Shift operator program

--- src/Shift_operator.java
+++ src/Shift_operator.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Shift_operator {
         public static void main(String[] args) {
-            int a = 10;
-            int b = 6;
+            Scanner Object= new Scanner(System.in);
 
-            System.out.println("b is " + (b>>1));
-            System.out.println("a is " + (a<<3));
+            System.out.print("\nEnter First Value: ");
+            int first = Object.nextInt();
+
+            System.out.print("\nEnter Second Value: ");
+            int second = Object.nextInt();
+
+            System.out.println("\n\n***************  MULTIPLYING USING lEFT SHIFT  *****************");
+            System.out.printf("\nMultiplying %d by 1 : " + (first<<0)+"\n", first);
+            System.out.printf("\nMultiplying %d by 2 : " + (first<<1)+"\n", first);
+            System.out.printf("\nMultiplying %d by 4 : " + (first<<2)+"\n", first);
+            System.out.printf("\nMultiplying %d by 8 : " + (first<<3)+"\n", first);
+
+            System.out.println("\n\n***************  DEVIDING USING RIGHT SHIFT  *****************");
+            System.out.printf("\nDividing %d by 1 : " + (second >> 0) + "\nRemainder: " + (second % 1)+"\n",second);
+            System.out.printf("\nDividing %d by 2 : " + (second >> 1) + "\nRemainder: " + (second % 2)+"\n",second);
+            System.out.printf("\nDividing %d by 4 : " + (second >> 2) + "\nRemainder: " + (second % 4)+"\n",second);
+            System.out.printf("\nDividing %d by 8 : " + (second >> 3) + "\nRemainder: " + (second % 8)+"\n",second);
+
         }
 
 }
