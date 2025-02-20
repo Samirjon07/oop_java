f94646282430fcc3579e8cb2e5e398829d878403
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-02-20 22:42:13 +0500

    Fahrenheit Commit

--- src/Ferin_celcius.java
+++ src/Ferin_celcius.java
@@ -1,3 +1,24 @@
-public class Ferin_celcius {
+import java.util.Scanner ;
 
+
+public class Ferin_celcius
+{
+    public static void main(String[] args)
+    {
+        Scanner Object= new Scanner(System.in);
+
+        final int Base = 32;
+        final double Conversion_Factor =9.0/5.0;
+
+        double fahr;
+
+        System.out.print("\nEnter Celsius Value: ");
+        int celsius = Object.nextInt();
+
+        fahr = celsius * Conversion_Factor + Base ;
+
+        System.out.println("\nCelsius Temperature: " + celsius + "\t Fahrenheit Temperature: " + fahr);
+
+
+    }
 }
