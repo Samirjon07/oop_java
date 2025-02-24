51927eb3cd3046fdd664e56555d42dd56ab51160
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-02-24 10:19:52 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 5;
         int b;
-        b=6 + a++;
+        b = 6 + a++;
         System.out.println("b is " + (b));
         System.out.println("a is " + (a++));
 
