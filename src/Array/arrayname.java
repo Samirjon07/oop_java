42d00efe7d1c6983995ca6d18f43f8a8ecf5d0d7
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-20 11:07:37 +0500

    Intarray

--- src/Array/arrayname.java
+++ src/Array/arrayname.java
@@ -1,6 +1,14 @@
 package Array;
 
 public class arrayname {
+
     String[][] university={{"1","2","3"},{"4","5","6"}};
+    String[] university1={"1","2","3"};
+
+
+    int[][] grades={{91,82,73},{64,55,76}};
+    int[] grades1={81,92,93};
+
 }
+
 
