5fe776c40e720dc13c143556e099702b53cb401d
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-21 11:34:20 +0500

    For each

--- src/Array/arrayname.java
+++ src/Array/arrayname.java
@@ -1,14 +1,33 @@
 package Array;
 
 public class arrayname {
+    public static void main(String[] args) {
 
-    String[][] university={{"1","2","3"},{"4","5","6"}};
-    String[] university1={"1","2","3"};
+        String[][] university = {{"1", "2", "3"}, {"4", "5", "6"}};
+        String[] university1 = {"1", "2", "3"};
+
+        int[] grades1 = {81, 92, 93};
+        int[][] grades = {{91, 82, 73}, {64, 55, 76}};
+
+        for (int i = 0; i < grades.length; i++) {
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
 
 
-    int[][] grades={{91,82,73},{64,55,76}};
-    int[] grades1={81,92,93};
+        for (int[] i : grades) {
+            for (int j : i) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
+
 
 
