acc707df7fd9f8d3a41a6e8dc5cf126e7ef682bb
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-22 11:47:29 +0500

    Do while

--- src/Array/arrayname.java
+++ src/Array/arrayname.java
@@ -3,13 +3,13 @@ package Array;
 public class arrayname {
     public static void main(String[] args) {
 
-        String[][] university = {{"1", "2", "3"}, {"4", "5", "6"}};
+        String[][] university = {{"Cs", "Ba", "mining"}, {"pe", "epe", "6"}};
         String[] university1 = {"1", "2", "3"};
 
         int[] grades1 = {81, 92, 93};
         int[][] grades = {{91, 82, 73}, {64, 55, 76}};
 
-        for (int i = 0; i < grades.length; i++) {
+       /* for (int i = 0; i < grades.length; i++) {
             for (int j = 0; j < grades[i].length; j++) {
                 System.out.print(grades[i][j]);
                 System.out.print(" ");
@@ -23,7 +23,21 @@ public class arrayname {
                 System.out.print(j + " ");
             }
             System.out.println();
-        }
+        }*/
+
+        int i=0;
+
+
+        do {
+            int j = 0;
+            do {
+                System.out.print(university[i][j] + " ");
+                j++;
+            } while (j < university[i].length);
+
+            System.out.println();
+            i++;
+        } while (i < university.length);
 
     }
 
